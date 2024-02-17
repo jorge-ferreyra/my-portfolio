c0b6344d3bc9bbacbf132a2aff0a5af8ec4d23ec
Jorge Ferreyra <jorxfer@gmail.com>
2024-02-17 14:05:08 -0300

    Adding tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			ocean: 'rgb(2, 0, 19)',
+			blackmatter: 'rgb(1, 0, 10)',
+			transparent: 'transparent',
+			graysmooth: 'rgba(255, 255, 255, .015)',
+			lightsea: 'rgb(148, 148, 255)',
+			boldsea: 'rgb(86, 86, 253)',
+			
+		},
+		extend: {},
+	},
+	plugins: [],
+}
