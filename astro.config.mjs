bbd4a639f44881b707f667e1c4dc5f9d65c2bc4e
Jorge Ferreyra <jorxfer@gmail.com>
2024-03-05 09:53:16 -0300

    Fixing repeated ids

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  optimize: {
+    styles: true,
+    scripts: true,
+    images: true,
+    preload: true,
+    http2: true,
+    compress: true,
+  }
 });
